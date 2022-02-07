e65e7fd73788369099bf03643970d662ce4afd0e
Blanca Matango <bmatango@cgbsatelital.com>
2022-02-06 23:37:33 -0500

    New CACHE_STATIC version

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ importScripts('js/sw-db.js');
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE    = 'static-v1';
+const STATIC_CACHE    = 'static-v4';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
